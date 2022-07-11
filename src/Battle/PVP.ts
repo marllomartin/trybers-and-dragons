c85ab5148e457564948d2d8d69edd1eae9d24d89
marllomartin <marllon3636@gmail.com>
2022-07-11 19:54:00 -0300

    refactor(Battle/PVP): Ajusta classe PVP

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -2,19 +2,21 @@ import Fighter from '../Fighter';
 import Battle from './Battle';
 
 class PVP extends Battle {
+  player1: Fighter;
   player2: Fighter;
 
-  constructor(player: Fighter, player2: Fighter) {
-    super(player);
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this.player1 = player1;
     this.player2 = player2;
   }
 
   fight(): number {
-    while (this.player.lifePoints > 0 && this.player2.lifePoints > 0) {
-      this.player.attack(this.player2);
-      this.player2.attack(this.player);
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
     }
-    return this.player.lifePoints === -1 ? -1 : 1;
+    return this.player1.lifePoints === -1 ? -1 : 1;
   }
 }
 export default PVP;
