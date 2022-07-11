3cd09f2feea2daba51b8e4b780e40bee3df6a2b4
marllomartin <marllon3636@gmail.com>
2022-07-11 19:55:22 -0300

    feat(src/index): Cria index principal

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Elesis');
+const player2 = new Character('Lire');
+const player3 = new Character('Arme');
+
+for (let index = 0; index < 25; index += 1) {
+  player1.levelUp();
+}
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]): void {
+  for (let index = 0; index < battles.length; index += 1) {
+    battles[index].fight();
+  }
+}
+
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
